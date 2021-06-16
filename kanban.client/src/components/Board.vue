7117a2cb978c994ec574eb376957dbe41f6a91c0
AlexMuir138 <muirkat138@gmail.com>
2021-06-16 17:45:56 -0600

    Board css

--- kanban.client/src/components/Board.vue
+++ kanban.client/src/components/Board.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="card col-3 text-center">
-    <i @click="deleteBoard" v-if="board.creatorId === account.id">X</i>
-    <router-link :to="{name: 'Board', params: {id: board.id}}" @click="setActiveBoard">
-      {{ board.name }}
+  <div class="card col-3 text-center rounded m-3">
+    <router-link :to="{name: 'Board', params: {id: board.id}}" @click="setActiveBoard" class="p-3 m-2 border shadow-lg">
+      <h2>{{ board.name }}</h2>
     </router-link>
+    <p class="p-2">
+      {{ board.email }}
+      <img :src="board.picture" :alt="board.email" class="rounded-pill" height="40" />
+    </p>
+    <div class="d-flex align-items-column justify-content-center text-danger shadow-lg m-3">
+      <p>Delete</p>
+      <i @click="deleteBoard" v-if="board.creatorId === account.id" class="mdi mdi-trash-can-outline"></i>
+    </div>
   </div>
 </template>
 
